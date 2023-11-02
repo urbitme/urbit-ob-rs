fd5b5e11c65ffbc0c902736a8fe45bdeb8f94e40
~nodreb-borrus <nodreb@borr.us>
2023-11-02 15:07:11 -0600

    Example to check patp speed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use num_bigint::BigUint;
 use urbit_ob::*;
+
+fn all_planets(star_val: u32) -> Vec<String> {
+    let mut p_names: Vec<String> = Vec::with_capacity(0xffff);
+    for value in 0x0001..=0xffff {
+        let p_val: u32 = (value << 16) | star_val;
+        let p_name = patp(&BigUint::from(p_val));
+        p_names.push(p_name);
+    }
+    p_names
+}
 
 fn main() {
     println!("{:?}", patp2dec("~rondev"));
@@ -28,4 +39,8 @@ fn main() {
     println!("{:?}", sein("~rondev-master-martyr-finned"));
     println!("{:?}", PREFIX_VALUES.get("nod"));
     // println!("{:?}", muk(0xee281300, 2, &BigUint::from(0xcafebabe_u64)));
+
+    for i in 24755..24765 {
+        println!("{:?}", all_planets(i).len())
+    }
 }
